c18b251faad280234736aada5fb7459a1a5944b6
DLIF\fih <fih@dlimi.com>
2025-05-20 14:56:41 +0200

    intro dialogue is working

--- scenes/arrow_to_start.gd
+++ scenes/arrow_to_start.gd
@@ -5,5 +5,4 @@ func _ready():
 
 func _on_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		print("clicked right arrow")
-		get_tree().change_scene_to_file("res://scenes/auditorium.tscn")
+		get_tree().change_scene_to_file("res://scenes/game.tscn")
--- singleton/singleton.gd
+++ singleton/singleton.gd
@@ -2,8 +2,13 @@ extends Node
 
 @onready var key: Area2D = $key
 
+var intro_played = false
+
 var inventory = {
 	"cup" = false,
 	"key" = false,
 	"flashlight" = false
 }
+
+func invoke_intro_played():
+	intro_played = true
