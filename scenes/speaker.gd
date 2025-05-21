641e4497fe86ac9335225d74b3496c287723e686
DLIF\fih <fih@dlimi.com>
2025-05-21 16:12:35 +0200

    added marketing speaker dialogue

--- scenes/speaker.gd
+++ scenes/speaker.gd
@@ -1,10 +1,10 @@
 extends Area2D
- 
+
 func _ready() -> void:
 	connect("input_event", _on_input_event)
 	input_pickable = true
- 
+
 func _on_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		var resource = preload("res://assets/dialogue/letter_opener.dialogue")
+		var resource = preload("res://assets/dialogue/marketing.dialogue")
 		DialogueManager.show_example_dialogue_balloon(resource)
