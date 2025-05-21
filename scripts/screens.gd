34067071e1681cc210c4b99487a17e9f46ce7992
DLIF\ice <ice@signumlifescience.com>
2025-05-21 13:55:22 +0200

    add signum site

---
+++ scripts/screens.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+func _ready():
+	connect("input_event", _on_input_event)
+
+func _on_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		get_tree().change_scene_to_file("res://scenes/signum_website.tscn")
