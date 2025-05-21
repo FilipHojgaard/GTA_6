4eb23d1d3a8d642df4a6f85c092e53a98e2272ea
DLIF\fih <fih@dlimi.com>
2025-05-21 09:53:09 +0200

    203 working without correct sprite

--- scenes/phone_start.gd
+++ scenes/phone_start.gd
@@ -3,7 +3,8 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if Singleton.intro_played == true:
+		queue_free()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
