901d8da3347ec57d21d1b293355f609f19be3dec
DLIF\fih <fih@dlimi.com>
2025-05-21 10:44:00 +0200

    coffee lounge and auditorium implemented

--- scenes/to_auditorium.gd
+++ scenes/to_auditorium.gd
@@ -6,4 +6,4 @@ func _ready():
 func _on_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		print("clicked right arrow")
-		get_tree().change_scene_to_file("res://scenes/coffee_lounge.tscn")
+		get_tree().change_scene_to_file("res://scenes/auditorium.tscn")
