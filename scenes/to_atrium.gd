0957db821984c52ee54650eaba70972239de0e9f
DLIF\ice <ice@signumlifescience.com>
2025-05-21 10:33:30 +0200

    left office

---
+++ scenes/to_atrium.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+func _ready():
+	connect("input_event", _on_input_event)
+
+func _on_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		print("clicked right arrow")
+		get_tree().change_scene_to_file("res://scenes/atrium.tscn")
