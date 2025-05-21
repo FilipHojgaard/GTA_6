2a79ea37760c785d133e7effb759a44b67721155
DLIF\fih <fih@dlimi.com>
2025-05-21 13:12:02 +0200

    keys are obtainable

--- scenes/keys_world.gd
+++ scenes/keys_world.gd
@@ -1,12 +1,10 @@
 extends Area2D
 
-signal cup_sig
-
 func _ready() -> void:
 	connect("input_event", _on_input_event)
 	input_pickable = true
 
 func _on_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		Singleton.inventory["cup"] = true
+		Singleton.inventory["key"] = true
 		visible = false
--- scripts/key_enlargen.gd
+++ scripts/key_enlargen.gd
@@ -12,6 +12,8 @@ func _process(delta: float) -> void:
 
 func _on_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		print("trying to enllargen")
-		var paper_instance = paper_scene.instantiate()
-		add_child(paper_instance)
+		var resource = preload("res://assets/dialogue/key_dialogue.dialogue")
+		DialogueManager.show_example_dialogue_balloon(resource)
+		#print("trying to enllargen")
+		#var paper_instance = paper_scene.instantiate()
+		#add_child(paper_instance)
