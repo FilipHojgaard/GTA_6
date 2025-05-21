208a4917c0c6210eb8d21dbed663e7311e29a8e6
DLIF\ice <ice@signumlifescience.com>
2025-05-21 14:22:08 +0200

    add DMVO info

--- scripts/cup.gd
+++ scripts/cup.gd
@@ -10,3 +10,6 @@ func _on_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		Singleton.inventory["cup"] = true
 		visible = false
+		
+		var resource = preload("res://assets/dialogue/drink.dialogue")
+		DialogueManager.show_example_dialogue_balloon(resource)
